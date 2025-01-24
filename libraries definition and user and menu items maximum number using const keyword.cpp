f849f2814764789b0c5ecd52bff54fc639d6bb41
abdullah-amjaddd <abdullazzak2005@gmail.com>
2025-01-24 14:39:20 +0500

    done

---
+++ libraries definition and user and menu items maximum number using const keyword.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <iomanip>
+#include <stdexcept>
+using namespace std;
+
+const int MAX_USERS = 100;
+const int MAX_MENU_ITEMS = 50;
